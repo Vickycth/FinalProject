6e0f70b61821397b0fd9877f61f8e50b76093681
Vicky <tianhui2@illinois.edu>
2018-12-08 01:59:57 -0600

    aa

--- app/src/main/java/com/example/android/finalproject/Cat.java
+++ app/src/main/java/com/example/android/finalproject/Cat.java
@@ -1,4 +1,29 @@
 package com.example.android.finalproject;
 
-public class Cat {
+public final class Cat {
+    String url;
+    String id;
+
+    public Cat(String url, String id) {
+        this.url = url;
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
 }
+
